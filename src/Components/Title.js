bdbde64098015513720a9ad2513ac3c35e8902e9
Redet Getachew <redetg@gmail.com>
2024-10-21 21:59:17 +0300

    misc: reduces chart title font size

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -4,7 +4,13 @@ import Typography from "@mui/material/Typography";
 
 function Title(props) {
   return (
-    <Typography component="h6" variant="h6" color="primary" gutterBottom>
+    <Typography
+      style={{ fontSize: 16 }}
+      component="h6"
+      variant="h6"
+      color="primary"
+      gutterBottom
+    >
       {props.children}
     </Typography>
   );
